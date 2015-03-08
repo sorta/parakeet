d97eb726b1bcfd533744aa65fcb24bd0f186431a
Sophie Orta <orta.sophie+gh@gmail.com>
2015-03-08 01:20:43 -0800

    more environment setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,36 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    plugins = gulpLoadPlugins(),
+    browserSync = require('browser-sync')
+    reload = browserSync.reload
+    mainBowerFiles = require('main-bower-files');
+
+gulp.task('styles', function () {
+    return gulp.src('src/styles/main.scss')
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass())
+        .pipe(plugins.autoprefixer({browsers: ['last 2 version']}))
+        .pipe(plugins.minifyCss())
+        .pipe(plugins.sourcemaps.write())
+        .pipe(gulp.dest('examples/styles'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('serve', ['styles', 'bowerCopy'], function() {
+    browserSync({
+        server: "./examples"
+    });
+
+    gulp.watch("src/styles/*.scss", ['styles']);
+    gulp.watch("examples/*.html").on('change', reload);
+    gulp.watch("bower.json", ['bowerCopy']);
+});
+
+gulp.task('bowerCopy', function () {
+    return gulp.src(mainBowerFiles())
+        .pipe(gulp.dest('examples/bower_components'));
+});
 
 gulp.task('default', function() {
-  // place code for your default task here
+    gulp.start('serve');
 });
